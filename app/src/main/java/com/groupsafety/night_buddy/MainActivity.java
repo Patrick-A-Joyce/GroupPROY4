de09c1dd53fd1500afcf0bfb58e9a8dbfee06707
G00316712 <g00316712@gmail.com>
2016-10-22 21:54:09 +0100

    Second Commit Reading & writing to Firebase

--- app/src/main/java/com/groupsafety/night_buddy/MainActivity.java
+++ app/src/main/java/com/groupsafety/night_buddy/MainActivity.java
@@ -1,13 +1,68 @@
 package com.groupsafety.night_buddy;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView mConditionTextView;
+    Button mButtonSunny;
+    Button mButtonCloudy;
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef = mRootRef.child("condition");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mConditionTextView = (TextView)findViewById(R.id.textViewCondition);
+        mButtonCloudy = (Button)findViewById(R.id.button_Cloudy);
+        mButtonSunny = (Button)findViewById(R.id.button_Sunny);
     }
+
+    protected void onStart(){
+        super.onStart();
+
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                mConditionTextView.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mButtonSunny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConditionRef.setValue("SUNNY");
+            }
+        });
+
+        mButtonCloudy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConditionRef.setValue("CLOUDY");
+            }
+        });
+
+    }
+
+
 }
